9719883871ca21a12ca30c887696ed39990dc3e4
Gabriel Komor <gabkomti@gmail.com>
2026-02-15 23:57:12 +0100

    update main file

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-# from interfaces.log_in_ui import run_app
-# from interfaces.main_window_ui import run_main
-
 from src.frontend.log_in_ui import run_app
 from src.frontend.main_window_ui import run_main
 
-
-run_app()
-# run_main()
+# run_app()
+run_main()
